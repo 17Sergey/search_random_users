112238c866b63e49dbc4934e1b2e3b6033ed2018
Sergey <sergeykachaliukdev@gmail.com>
2024-10-07 16:05:48 +0300

    fix: deleted on-purpose error in api url

--- src/api/usersAPI.ts
+++ src/api/usersAPI.ts
@@ -2,7 +2,7 @@ import { formatUsers } from "../utils/formatUsers";
 import { searchUsersByName } from "../utils/searchUsersByName";
 
 const getAll = async () => {
-    const response = await fetch(`https://randomuser.me/ap/?results=100&seed=foobar`);
+    const response = await fetch(`https://randomuser.me/api/?results=100&seed=foobar`);
     if (!response.ok) throw new Error(`Oops...Error ${response.status}`);
 
     const data = await response.json();
